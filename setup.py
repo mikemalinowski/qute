84b6af697991235fa1345585ced39ceaec205364
mikemalinowski <mike.malinowski@outlook.com>
2019-03-25 21:03:53 +0000

    Release menu generation via dictionary

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 setuptools.setup(
     name='qute',
-    version='1.0.3',
+    version='1.0.4',
     author='Mike Malinowski',
     author_email='mike@twisted.space',
     description=short_description,
